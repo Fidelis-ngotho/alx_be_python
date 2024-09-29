0c82cf587f325ad0257af9db5ec69355f37c4e31
fidelis-ngotho <fidelngotho2@gmail.com>
2024-09-29 12:27:37 +0300

    Add temp_conversion_tool.py for temperature conversions using global variables

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,8 +1,7 @@
-# Global Conversion Factors
+
 FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9
 CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5
 
-# Conversion Functions
 def convert_to_celsius(fahrenheit):
     """Convert Fahrenheit to Celsius using the global conversion factor."""
     return (fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
@@ -11,7 +10,7 @@ def convert_to_fahrenheit(celsius):
     """Convert Celsius to Fahrenheit using the global conversion factor."""
     return celsius * CELSIUS_TO_FAHRENHEIT_FACTOR + 32
 
-# User Interaction
+
 def main():
     try:
         temp = float(input("Enter the temperature to convert: "))
